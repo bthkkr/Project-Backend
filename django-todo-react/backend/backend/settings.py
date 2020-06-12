1d7b37b5e48e86bff5f34ea721c18e03b6933502
bijzpriz <bijalrtd@gmail.com>
2020-06-12 15:14:09 -0500

    deploy

--- django-todo-react/backend/backend/settings.py
+++ django-todo-react/backend/backend/settings.py
@@ -14,8 +14,8 @@ import os
 # import django_heroku
 
 import dj_database_url 
-db_from_env = dj_database_url.config()
-DATABASES[‘default’].update(db_from_env)
+
+db_from_env = dj_database_url.config(conn_max_age=600)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
