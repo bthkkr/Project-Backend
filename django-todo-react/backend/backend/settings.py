cf034bf6c02571b19f8b80e9d1f31ca875553ec4
bijzpriz <bijalrtd@gmail.com>
2020-06-12 15:11:32 -0500

    deploy

--- django-todo-react/backend/backend/settings.py
+++ django-todo-react/backend/backend/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 # import django_heroku
 
-import dj-database-url 
+import dj_database_url 
 db_from_env = dj_database_url.config()
 DATABASES[‘default’].update(db_from_env)
 
